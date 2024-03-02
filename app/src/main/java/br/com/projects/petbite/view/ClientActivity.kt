92d97cf85816f460cf8b338be2455679c24eb4f9
lucasnichals <127261783+lucasnichals@users.noreply.github.com>
2024-03-02 17:38:53 -0300

    Register screen layout done

---
+++ app/src/main/java/br/com/projects/petbite/view/ClientActivity.kt
@@ -0,0 +1,12 @@
+package br.com.projects.petbite.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.projects.petbite.R
+
+class ClientActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_client)
+    }
+}
