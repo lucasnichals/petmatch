bab06a52b2c2e82b14b4625ac7d411ea00f2b6d3
lucasnichals <127261783+lucasnichals@users.noreply.github.com>
2024-03-02 18:34:32 -0300

    Register screen layout done

--- app/src/main/java/br/com/projects/petbite/view/MainActivity.kt
+++ app/src/main/java/br/com/projects/petbite/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.projects.petbite
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,12 +8,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import br.com.projects.petbite.view.HomeActivity
+import br.com.projects.petbite.view.ClientActivity
+
+
 
 class MainActivity : AppCompatActivity() {
     val USERNAME = "u"
     val PASSWORD = "p"
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,27 +25,42 @@ class MainActivity : AppCompatActivity() {
         val btnLogin: Button = findViewById(R.id.btn_main_login)
         val tvCadastro: TextView = findViewById(R.id.tv_main_cadastro)
 
-            btnLogin.setOnClickListener {
-                val enteredUsername = etUsername.text.toString()
-                val enteredPassword = etPassword.text.toString()
-                if (enteredUsername == USERNAME && enteredPassword == PASSWORD) {
-                    showToast("Login bem-sucedido", Toast.LENGTH_LONG)
-                } else {
-                    showToast("Nome de usuário ou senha incorretos", Toast.LENGTH_LONG)
-                }
+        btnLogin.setOnClickListener {
+            val enteredUsername = etUsername.text.toString()
+            val enteredPassword = etPassword.text.toString()
+            if (enteredUsername == USERNAME && enteredPassword == PASSWORD) {
+                showToast("Login bem-sucedido", Toast.LENGTH_LONG)
+            } else {
+                showToast("Nome de usuário ou senha incorretos", Toast.LENGTH_LONG)
             }
+        }
+
 
         tvCadastro.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
+            val intent = Intent(this, ClientActivity::class.java)
             startActivity(intent)
-            } }
-        private fun showToast(message: String, duration: Int) {
-            val toast = Toast.makeText(this, message, duration)
-            toast.setGravity(
-                android.view.Gravity.TOP or android.view.Gravity.CENTER_HORIZONTAL,
-                10,
-                10
-            )
-            toast.show()
+        }
+
+        fun showToast(message: String, duration: Int) {
+            runOnUiThread {
+                Toast.makeText(this, message, duration).show()
+            }
+        }
+
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, PetListActivity::class.java)
+            startActivity(intent)
+
         }
     }
+
+    private fun showToast(message: String, duration: Int) {
+        val toast = Toast.makeText(this, message, duration)
+        toast.setGravity(
+            android.view.Gravity.TOP or android.view.Gravity.CENTER_HORIZONTAL, 10, 10
+        )
+        toast.show()
+    }
+
+}
