77e66817440f5fdf5ba2904d65780a02b41c89dc
lucasnichals <127261783+lucasnichals@users.noreply.github.com>
2024-03-15 19:19:38 -0300

    pet register screen ok

--- app/src/main/java/br/com/projects/petbite/MyFirebaseMessagingService.kt
+++ app/src/main/java/br/com/projects/petbite/MyFirebaseMessagingService.kt
@@ -1,6 +1,5 @@
-package br.com.projects.petbite
+package br.com.projects.petbite.viewmodel
 
-import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -9,8 +8,12 @@ import android.content.Intent
 import android.os.Build
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
+import br.com.projects.petbite.MainActivity
+import br.com.projects.petbite.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+
+
 
 const val channelId = "notification_channel"
 const val channelName = "br.com.projects.petbite"
@@ -37,14 +40,12 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         return remoteView
     }
 
-    @SuppressLint("UnspecifiedImmutableFlag")
     fun generateNotification(tittle: String, message: String) {
 
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-
-        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
-
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
         var builder: NotificationCompat.Builder =
             NotificationCompat.Builder(applicationContext, channelId)
                 .setSmallIcon(R.drawable.ic_logo)
@@ -54,15 +55,12 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
                 .setContentIntent(pendingIntent)
 
         builder = builder.setContent(getRemoteView(tittle, message))
-
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel =
                 NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
             notificationManager.createNotificationChannel((notificationChannel))
-
         }
         notificationManager.notify(0, builder.build())
     }
